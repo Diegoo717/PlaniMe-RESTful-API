5a7f0e5d431aa47344b842420cbd90353bc4a6ec
Diego <soydiegoo71@gmail.com>
2025-06-16 23:57:58 -0600

    feat: add initial Resend service configuration
    Integrated basic Resend email service setup in the new services folder.
    Configured environment variables and base email sending function.

---
+++ services/resend.js
@@ -0,0 +1,23 @@
+require("dotenv").config;
+const { Resend } = require("resend");
+
+function resend(email, fullName, date, time, service, code) {
+  const resend = new Resend(process.env.API_KEY_RESEND);
+
+  (async function () {
+    const { data, error } = await resend.emails.send({
+      from: "HairRush <soport@" + process.env.DOMAIN + ">",
+      to: [email],
+      subject: "",
+      html: ``,
+    });
+
+    if (error) {
+      return console.error({ error });
+    }
+
+    console.log({ data });
+  })();
+}
+
+module.exports = { resend };
